0c9e54b289aac31f8550f880097a627ea6f99454
naufalbayazid <naufalichwanbayazid@gmail.com>
2023-04-10 14:43:51 +0700

    masukkkan data mhs

---
+++ class_oop_192/class_oop_192.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	string nama;
+	void tampil() {
+		cout << "NIM= " << nim;
+		cout << "\bNama= " << nama;
+	}
+
+};
